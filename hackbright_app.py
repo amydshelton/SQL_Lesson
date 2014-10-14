88d2050a2656b3e7ed63c0093b1ba63d8345e3ed
Hackbright Student <info@hackbrightacademy.com>
2014-10-14 12:27:37 -0700

    Everything is working, waiting for a code review.

--- hackbright_app.py
+++ hackbright_app.py
@@ -27,9 +27,30 @@ def make_new_project(project_title, description, max_grade):
     CONN.commit()
     print "Successfully added project: %s" % project_title
 
-#def get_student_project_grade():
-#    query = """SELECT 
+def get_student_project_grade(last_name, project_title):
+    query = """SELECT grade FROM Grades JOIN Students ON (Grades.student_github = Students.github) WHERE last_name = ? AND project_title = ?"""
+    DB.execute(query, (last_name, project_title))
+    row = DB.fetchone()
+    print """Student: %s got %d on %s.""" % (last_name, row[0], project_title) 
 
+def give_student_grade(last_name, project_title, grade):
+    query = """SELECT github FROM Students WHERE last_name = (?)"""
+    DB.execute(query, (last_name,))
+    github = DB.fetchone()[0]
+    query = """INSERT INTO Grades(student_github, project_title, grade) VALUES (?,?,?)"""
+    DB.execute(query, (github, project_title, grade))
+    CONN.commit()
+    print "Successfully added grade: %s." % grade
+
+def return_student_grades(last_name):
+    query = """SELECT github FROM Students WHERE last_name = (?)"""
+    DB.execute(query, (last_name,))
+    github = DB.fetchone()[0]
+    query = """SELECT project_title, grade FROM Grades WHERE student_github = ?"""
+    DB.execute(query, (github,))
+    rows = DB.fetchall()
+    for project in rows:
+        print "On project %s, %s got %d" % (str(project[0]), last_name, project[1])
 
 def connect_to_db():
     global DB, CONN
@@ -41,23 +62,30 @@ def main():
     command = None
     while command != "quit":
         input_string = raw_input("HBA Database> ")
-        tokens = input_string.split()
+        tokens = input_string.split(',')
        # print tokens
-        command = tokens[0]
+        command = tokens[0].strip()
         args = tokens[1:]
+        newargs = []
+        for arg in args:
+            arg = arg.strip()
+            newargs.append(arg)
 
         if command == "student":
-           # print "ehy!"
-            get_student_by_github(*args) 
-        elif command == "new_student":
-            make_new_student(*args)
+            get_student_by_github(*newargs) 
+        elif command == "new student":
+            make_new_student(*newargs)
         elif command == "project":
             get_project()
-        elif command == "new_project":
-            make_new_project(*args)
- #       elif command == "student_project_grade":
- #           get_student_project_grade(*args)
- #working on queryiing for a student's grade given a projects
+        elif command == "new project":
+            make_new_project(*newargs)
+        elif command == "student project grade":
+            get_student_project_grade(*newargs)
+        elif command == "give grade":
+            give_student_grade(*newargs)
+        elif command == "student grades":
+            return_student_grades(*newargs)
+ 
 
     CONN.close()
 
